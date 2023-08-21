0058e99de4cd2ba0dd537328161a316b790db496
Иван Бахтин <vanobl@mail.ru>
2023-08-21 17:27:32 +0300

    очередные изменения

--- src/services/reducers/rootReducer.js
+++ src/services/reducers/rootReducer.js
@@ -1,8 +1,9 @@
 import { combineReducers } from "redux";
 import totalSum from "./totalSum";
 import orderNumber from "./orderNumber";
+import ingredientsList from "./ingredientsList";
 
 // Корневой редьюсер
-const rootReducer = combineReducers({totalSum, orderNumber});
+const rootReducer = combineReducers({totalSum, orderNumber, ingredientsList});
 
 export default rootReducer;
