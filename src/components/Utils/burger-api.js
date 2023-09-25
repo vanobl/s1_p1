1b8046a97562a8a8d89fd6198a968a33377acd53
Бахтин Иван <vanobl@mail.ru>
2023-09-25 17:03:18 +0300

    изменён набор api

--- src/components/Utils/burger-api.js
+++ src/components/Utils/burger-api.js
@@ -1,5 +1,7 @@
 const NORMA_API = "https://norma.nomoreparties.space";
 const NORMA_API_CREATE_ORDER = 'https://norma.nomoreparties.space/api/orders';
+const NOMA_API_FORGOT_PASSWORD = 'https://norma.nomoreparties.space/api/password-reset';
+const NOMA_API_RESET_PASSWORD = 'https://norma.nomoreparties.space/api/password-reset/reset';
 
 export async function getIngredients() {
   try {
@@ -32,6 +34,47 @@ export async function createOrder(ingredientsList) {
     return content;
   } catch (err) {
     alert("Ошибка оформления заказа " + err);
+  }
+}
+
+export async function forgotPassword(forgotEmail) {
+  try {
+    const response = await fetch(NOMA_API_FORGOT_PASSWORD, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({email: forgotEmail})
+    });
+
+    status(response);
+
+    const content = await response.json();
+
+    return content;
+  } catch (err) {
+    alert("Ошибка отправки восстановительного письма " + err);
+  }
+}
+
+export async function resertPassword(newPassword, newToken) {
+  try {
+    const response = await fetch(NOMA_API_RESET_PASSWORD, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({password: newPassword, token: newToken})
+    });
+
+    status(response);
+
+    const content = await response.json();
+    return content
+  } catch (err) {
+    alert("Ошибка сброса пароля " + err);
   }
 }
 
